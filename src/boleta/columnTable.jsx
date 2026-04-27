1e9343cc80201e3413cecb87f715d2ac3811276c
systemsucre <systemsucre@gmail.com>
2026-04-26 20:20:10 -0400

    actualizacion del modulo de ingresso

--- src/boleta/columnTable.jsx
+++ src/boleta/columnTable.jsx
@@ -1,110 +1,139 @@
 export const ColumnsTable = [
-    {
-        label: 'NUM. BOLETA',
-        field: 'numero',
-        render: (row) => (
-            <div style={{ minWidth: '10px' }}>
-                <div className="fw-bold text-dark ">{row.numero_boleta}</div>
+  {
+    label: 'NUM. BOLETA',
+    field: 'numero',
+    render: (row) => (
+      <div style={{ minWidth: '10px', fontSize: '0.85rem' }}>
+        <div className="fw-bold text-dark ">{row.numero_boleta}</div>
+      </div>
+    ),
+    sortable: true,
+  },
 
-            </div>
-        ),
-        sortable: true,
+  {
+    label: 'CODIGO BOLETA',
+    field: 'codigo',
+    render: (row) => (
+      <div style={{ minWidth: '10px' }}>
+        <div className="fw-bold text-dark " style={{ fontSize: '0.85rem' }}>
+          {row.codigo_boleta}
+        </div>
+      </div>
+    ),
+    sortable: true,
+  },
+
+  // {
+  //     label: 'NUMERO BOLETA',
+  //     field: 'numero',
+  //     render: (row) => (
+  //         <div style={{ minWidth: '10px' }}>
+  //             <div className="fw-bold text-dark text-center ">{row.numero_boleta}</div>
+
+  //         </div>
+  //     ),
+  //     sortable: true,
+  // },
+
+  {
+    label: 'Fecha Registro',
+    field: 'created_at',
+    render: (row) => {
+      const fecha = new Date(row.fecha?.split(' ')[0]);
+      return (
+        <div className="small text-secondary" style={{ fontSize: '0.85rem' }}>
+          <i
+            className="bi bi-calendar3 me-1"
+            style={{ fontSize: '0.75rem' }}
+          ></i>
+          {row.fecha?.split('T')[0]}
+          <br />
+          <span className="text-muted" style={{ fontSize: '0.8rem' }}>
+            {fecha.toLocaleTimeString('es-BO', {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </span>
+        </div>
+      );
     },
+  },
+  {
+    label: 'Items',
+    field: 'items',
+    render: (row) => (
+      <div className="text-center">
+        <span className="fw-bold text-dark" style={{ fontSize: '0.85rem' }}>
+          {row.total_items}
+        </span>
+      </div>
+    ),
+  },
+  {
+    label: 'Monto',
+    field: 'monto',
+    render: (row) => (
+      <div className="text-center">
+        <span className="fw-bold text-dark" style={{ fontSize: '0.85rem' }}>
+          {row.simbolo} {row.monto_total}
+        </span>
+      </div>
+    ),
+  },
+  {
+    label: 'Usuario Registrador',
+    field: 'solicitante',
+    render: (row) => (
+      <div style={{ minWidth: '10px' }}>
+        <small className="text-muted" style={{ fontSize: '0.85rem' }}>
+          {row.solicitado_por}
+        </small>
+      </div>
+    ),
+    sortable: true,
+  },
+  {
+    label: 'Estado',
+    field: 'estado',
+    render: (row) => {
+      const estados = {
+        1: {
+          badge: 'bgss-secsondary text-secondary',
+          texto: 'SOLICITADO',
+          icon: 'bi-hourglass-split',
+        },
+        2: {
+          badge: 'bg-infos text-info',
+          texto: 'APROBADO',
+          icon: 'bi-check-circle',
+        },
+        3: {
+          badge: 'tex-success text-success',
+          texto: 'REGISTRADO',
+          icon: 'bi-cash-stack',
+        },
+        4: {
+          badge: 'bg-danger text-danger',
+          texto: 'RECHAZADO',
+          icon: 'bi-x-circle',
+        },
+      };
 
-    {
-        label: 'CODIGO BOLETA',
-        field: 'codigo',
-        render: (row) => (
-            <div style={{ minWidth: '10px' }}>
-                <div className="fw-bold text-dark ">{row.codigo_boleta}</div>
+      const est = estados[row.estado] || {
+        badge: 'bg-secondary',
+        texto: 'DESCONOCIDO',
+        icon: 'bi-question',
+      };
 
-            </div>
-        ),
-        sortable: true,
+      return (
+        <span
+          className={`badge ${est.badge} d-flex align-items-center w-fit-content px-2 py-1 `}
+          style={{ fontSize: '0.85rem', fontWeight: '600' }}
+        >
+          <i className={`bi ${est.icon} me-1`}></i>
+          {est.texto}
+        </span>
+      );
     },
-
-    // {
-    //     label: 'NUMERO BOLETA',
-    //     field: 'numero',
-    //     render: (row) => (
-    //         <div style={{ minWidth: '10px' }}>
-    //             <div className="fw-bold text-dark text-center ">{row.numero_boleta}</div>
-
-    //         </div>
-    //     ),
-    //     sortable: true,
-    // },
-
-    {
-        label: 'Fecha Registro',
-        field: 'created_at',
-        render: (row) => {
-            const fecha = new Date(row.fecha?.split(" ")[0])
-            return (
-                <div className="small text-secondary">
-                    <i className="bi bi-calendar3 me-1"></i>
-                    {row.fecha?.split("T")[0]}
-                    <br />
-                    <span className="text-muted" style={{ fontSize: '0.7rem' }}>
-                        {fecha.toLocaleTimeString('es-BO', { hour: '2-digit', minute: '2-digit' })}
-                    </span>
-                </div>
-            );
-        }
-    },
-    {
-        label: 'Items',
-        field: 'items',
-        render: (row) => (
-            <div className="text-center">
-                <span className="fw-bold text-dark" style={{ fontSize: '1.05rem' }}>
-                    {row.total_items}
-                </span>
-            </div>
-        )
-    },
-    {
-        label: 'Monto',
-        field: 'monto',
-        render: (row) => (
-            <div className="text-center">
-                <span className="fw-bold text-dark" style={{ fontSize: '1.05rem' }}>
-                    {row.simbolo} {row.monto_total}
-                </span>
-            </div>
-        )
-    },
-    {
-        label: 'Usuario Registrador',
-        field: 'solicitante',
-        render: (row) => (
-            <div style={{ minWidth: '10px' }}>
-                <small className="text-muted" style={{ fontSize: '0.9rem' }}>
-                    {row.solicitado_por}
-                </small>
-            </div>
-        ),
-        sortable: true,
-    },
-    {
-        label: 'Estado',
-        field: 'estado',
-        render: (row) => {
-            const estados = {
-                1: { badge: 'bgss-secondary text-dark', texto: 'SOLICITADO', icon: 'bi-hourglass-split' },
-                2: { badge: 'bg-info text-white', texto: 'APROBADO', icon: 'bi-check-circle' },
-                3: { badge: 'bg-success text-white', texto: 'REGISTRADO', icon: 'bi-cash-stack' },
-                4: { badge: 'bg-danger text-white', texto: 'RECHAZADO', icon: 'bi-x-circle' }
-            };
-
-            const est = estados[row.estado] || { badge: 'bg-secondary', texto: 'DESCONOCIDO', icon: 'bi-question' };
-
-            return (
-                <span className={`badge ${est.badge} d-flex align-items-center w-fit-content px-2 py-1`}>
-                    <i className={`bi ${est.icon} me-1`}></i>
-                    {est.texto}
-                </span>
-            );
-        }
-    }
+  },
 ];
--- src/tramite/columnTableTramites.jsx
+++ src/tramite/columnTableTramites.jsx
@@ -1,106 +1,153 @@
 export const ColumnsTableTramites = [
-    {
-        label: 'Numero',
-        field: 'numero,',
-        render: (row) =>
-            <div> <span className="fw-bold text-primary">{row.numero}</span></div>
-    },
-    {
-        label: 'Código de Caja',
-        field: 'codigo',
-        render: (row) =>
-            <div> <span className="fw-bold text-primary">{row.codigo}</span></div>
-    },
-    {
-        label: 'MONEDA',
-        field: 'sinmbolo',
-        render: (row) =>
-            <div> <span className="badge bg-light text-dark border">{row.simbolo}</span></div>
-    },
-    {
-        label: 'Detalle',
-        field: 'detalle',
-        render: (row) =>
-            <small className="text-muted italic" >
-                {row.detalle?.substring(0, 30)}...
-            </small>
-    },
+  {
+    label: 'Numero',
+    field: 'numero,',
+    render: (row) => (
+      <div>
+        {' '}
+        <span className="fw-bold text-primary" style={{ fontSize: '0.85rem' }}>
+          {row.numero}
+        </span>
+      </div>
+    ),
+  },
+  {
+    label: 'Código de Caja',
+    field: 'codigo',
+    render: (row) => (
+      <div>
+        {' '}
+        <span className="fw-bold text-primary" style={{ fontSize: '0.85rem' }}>
+          {row.codigo}
+        </span>
+      </div>
+    ),
+  },
+  {
+    label: 'MONEDA',
+    field: 'sinmbolo',
+    render: (row) => (
+      <div>
+        {' '}
+        <span
+          className="badge bg-light text-dark border"
+          style={{ fontSize: '0.85rem' }}
+        >
+          {row.simbolo}
+        </span>
+      </div>
+    ),
+  },
+  {
+    label: 'Detalle',
+    field: 'detalle',
+    render: (row) => (
+      <small className="text-muted italic">
+        {row.detalle?.substring(0, 30)}...
+      </small>
+    ),
+  },
 
-    {
-        label: 'Tipo Caja',
-        field: 'nombre_tipo_tramite',
-        render: (row) => (
-            <span className="badge bg-light text-dark border">
-                {row.nombre_tipo_tramite?.toUpperCase()}
-            </span>
-        )
-    },
-    {
-        label: 'Estado',
-        field: 'estado',
-        render: (row) => (<>
-            <span className={`badge ${row.estado === 1 ? 'bg-success' : 'bg-warning text-dark'}`}>
-                {row.estado === 1 ? 'EN CURSO' : row.estado === 2 ? 'PARALIZADO' : 'FINALIZADO'}
-            </span> <br />
-            <span className={`badge ${row.eliminado === 0 ? 'bg-danger' : 'bg-warning text-dark'}`}>
-                {row.eliminado === 0 ? 'Eliminado' : ''}
-            </span>
-        </>
-        )
-    },
-    {
-        label: 'Plazo y Fechas',
-        field: 'fecha_finalizacion',
-        render: (row) => {
-            const hoy = new Date();
-            const vencimiento = new Date(row.fecha_finalizacion);
-            const diasRestantes = Math.ceil((vencimiento - hoy) / (1000 * 60 * 60 * 24));
+  {
+    label: 'Tipo Caja',
+    field: 'nombre_tipo_tramite',
+    render: (row) => (
+      <span
+        className="badge bg-light text-dark border"
+        style={{ fontSize: '0.85rem' }}
+      >
+        {row.nombre_tipo_tramite?.toUpperCase()}
+      </span>
+    ),
+  },
+  {
+    label: 'Estado',
+    field: 'estado',
+    render: (row) => (
+      <>
+        <span
+          className={`badge ${row.estado === 1 ? 'text-success' : 'bg-warnsing text-warning'}`}
+          style={{ fontSize: '0.85rem' }}
+        >
+          {row.estado === 1
+            ? 'EN CURSO'
+            : row.estado === 2
+              ? 'PARALIZADO'
+              : 'FINALIZADO'}
+        </span>{' '}
+        <br />
+        <span
+          className={`badge ${row.eliminado === 0 ? 'text-danger' : 'bg-warnsing text-warning'}`}
+          style={{ fontSize: '0.85rem' }}
+        >
+          {row.eliminado === 0 ? 'Eliminado' : ''}
+        </span>
+      </>
+    ),
+  },
+  {
+    label: 'Plazo y Fechas',
+    field: 'fecha_finalizacion',
+    render: (row) => {
+      const hoy = new Date();
+      const vencimiento = new Date(row.fecha_finalizacion);
+      const diasRestantes = Math.ceil(
+        (vencimiento - hoy) / (1000 * 60 * 60 * 24),
+      );
 
-            let color = 'text-success';
-            if (diasRestantes <= 7) color = 'text-warning fw-bold';
-            if (diasRestantes <= 3) color = 'text-danger fw-bold animate-pulse';
+      let color = 'text-success';
+      if (diasRestantes <= 7) color = 'text-warning fw-bold';
+      if (diasRestantes <= 3) color = 'text-danger fw-bold animate-pulse';
 
-            return (
-                <div>
-                    <div className="small text-muted">
-                        <i className="bi bi-calendar-check me-1"></i>
-                        Apertura: {new Date(row.fecha_ingreso).toLocaleDateString()}
-                    </div>
-                    <div className="small text-info">
-                        <i className="bi bi-calendar-x me-1"></i>
-                        Cierre estimado: {vencimiento.toLocaleDateString()}
-                    </div>
-                    <div className={`${color} mt-1`} style={{ fontSize: '0.85rem' }}>
-                        <i className="bi bi-clock-history me-1"></i>
-                        {diasRestantes > 0 ? `${diasRestantes} días restantes` : 'Plazo vencido'}
-                    </div>
-                </div>
-            );
-        }
+      return (
+        <div>
+          <div className="small text-muted" style={{ fontSize: '0.85rem' }}>
+            <i className="bi bi-calendar-check me-1"></i>
+            Apertura: {new Date(row.fecha_ingreso).toLocaleDateString()}
+          </div>
+          <div className="small text-info" style={{ fontSize: '0.85rem' }}>
+            <i className="bi bi-calendar-x me-1"></i>
+            Cierre estimado: {vencimiento.toLocaleDateString()}
+          </div>
+          <div className={`${color} mt-1`} style={{ fontSize: '0.85rem' }}>
+            <i className="bi bi-clock-history me-1"></i>
+            {diasRestantes > 0
+              ? `${diasRestantes} días restantes`
+              : 'Plazo vencido'}
+          </div>
+        </div>
+      );
     },
-    {
-        label: 'Movimientos',
-        field: 'costo',
-        render: (row) => {
-            const costo = row.costo || 0;
-            return (
-                <div className="text-end">
-                    <div className="fw-bold text-dark">
-                        GASTOS : {row.simbolo} {row.total_gastos}
-                    </div>
-                    {/* {localStorage.getItem('numRol') != 4 ?
+  },
+  {
+    label: 'Movimientos',
+    field: 'costo',
+    render: (row) => {
+      const costo = row.costo || 0;
+      return (
+        <div className="text-end">
+          <div className="fw-bold text-dark" style={{ fontSize: '0.85rem' }}>
+            GASTOS : {row.simbolo} {row.total_gastos}
+          </div>
+          {/* {localStorage.getItem('numRol') != 4 ?
                         <div className=" fw-bold text-muted text-success italic" style={{ fontSize: '0.7rem' }}>
                             COSTO TRAMITE  Bs. {costo}
                         </div>
                         : null} */}
-                    <div className=" fw-bold text-muted text-success italic" style={{ fontSize: '0.7rem' }}>
-                        MONTO ABONADO  {row.simbolo} {row.total_ingresos} 
-                    </div>
-                    <div className={`fw-bold ${row.saldoDisponible > 2000 ? `text-dark` : row.saldoDisponible > 1000 ? `text-warning` : `text-danger`}`} style={{ fontSize: '0.7rem' }}>
-                        SALDO DISP.  {row.simbolo} {row.saldoDisponible}
-                    </div>
-                </div>
-            );
-        }
-    }
+          <div
+            className=" fw-bold text-muted text-success italic"
+            style={{ fontSize: '0.7rem' }}
+          >
+            MONTO ABONADO {row.simbolo} {row.total_ingresos}
+          </div>
+          <div
+            className={`fw-bold ${row.saldoDisponible > 2000 ? `text-dark` : row.saldoDisponible > 1000 ? `text-warning` : `text-danger`}`}
+            style={{ fontSize: '0.7rem' }}
+          >
+            SALDO DISP. {row.simbolo} {row.saldoDisponible}
+          </div>
+        </div>
+      );
+    },
+  },
 ];
